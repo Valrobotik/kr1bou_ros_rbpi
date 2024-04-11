18317574cd7cd91f62084f8717c2a68bae1be9e1
Valrobotik-INSA-HDF <82307338+Valrobotik-INSA-HDF@users.noreply.github.com>
2024-04-11 16:53:55 +0200

    Modif type Starter_robot.py

--- script/starter_robot.py
+++ script/starter_robot.py
@@ -4,18 +4,18 @@ from gpiozero import Button
 from signal import pause
 import rospy
 
-# Lorsque la clé est insérée, le robot envoi à un topic ROS un message indiquant que la clé est insérée (1)
+# Lorsque la clé est insérée, le robot envoi à un topic ROS un message indiquant que la clé est insérée (type booléen, True si la clé est insérée, False sinon)
 def talker(key):
-    pub = rospy.Publisher('starter', int, queue_size=10)
+    pub = rospy.Publisher('starter', Bool, queue_size=10)
     rospy.init_node('starter_talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         if key.is_pressed:
-            rospy.loginfo(1)
-            pub.publish(1)
+            rospy.loginfo(True)
+            pub.publish(True)
         else:
-            rospy.loginfo(0)
-            pub.publish(0)
+            rospy.loginfo(False)
+            pub.publish(False)
         rate.sleep()
 
 if __name__ == '__main__':
