1e7257431610edd4f5b6ad644cd647d2b5affeb2
Valrobotik-INSA-HDF <82307338+Valrobotik-INSA-HDF@users.noreply.github.com>
2024-04-12 17:44:29 +0200

    test run arduino via rbpi

---
+++ script/run_arduino_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import rospy # type: ignore
+from geometry_msgs.msg import Pose2D # type: ignore
+import serial # type: ignore
+import serial.tools.list_ports # type: ignore
+
+pose = Pose2D()
+
+def connect():
+    print(list(serial.tools.list_ports.comports()))
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    return ser
+    
+if __name__ == '__main__':
+    try:
+        ser = connect()
+        rospy.init_node('run_arduino_test', anonymous=True)
+        ser.write(b'1')
+        while not rospy.is_shutdown(): pass
+    except rospy.ROSInterruptException:
+        pass
