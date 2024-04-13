db00befc81183658ac5b149ce3f0dc14c36cca76
Paul Giroux <paul.giroux87@gmail.com>
2024-04-13 18:48:40 +0200

    test publish pose2D

--- script/talker_test.py
+++ script/talker_test.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Pose2D # type: ignore
+
+Pose2D()
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('chatter', Pose2D, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        
+        hello_str = Pose2D()
+        hello_str.x = 2.1
+        hello_str.y = 0.24
+        hello_str.theta = 3.14
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
