04894ee762443ef280984a36149b20b9172762d6
Valrobotik-INSA-HDF <82307338+Valrobotik-INSA-HDF@users.noreply.github.com>
2024-04-15 12:22:43 +0200

    rest input buffer

--- script/arduino_motor_node.py
+++ script/arduino_motor_node.py
@@ -28,6 +28,7 @@ def receive_odometry(hey):
     global ser, pub
     position = Pose2D()
     data = str(ser.read_until(b'R')).replace('b', '').replace("'", '').replace('\\r\\n', '').replace('R', '')
+    ser.reset_input_buffer()
     print(data)
     data = data.split(',')
     try:
