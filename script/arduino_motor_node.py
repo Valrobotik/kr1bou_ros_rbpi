ceb446fe389ed571b39ea294280d361607a33fd0
Valrobotik-INSA-HDF <82307338+Valrobotik-INSA-HDF@users.noreply.github.com>
2024-04-15 10:13:32 +0200

    update corection erreur , to ;

--- script/arduino_motor_node.py
+++ script/arduino_motor_node.py
@@ -49,13 +49,13 @@ def receive_odometry():
 
 def send_speed(speed: Twist):
     global ser
-    ser.write(f"V{speed.linear.x},{speed.angular.z}\n".encode())
-    print(f"V{speed.linear.x},{speed.angular.z}\n")
+    ser.write(f"V{speed.linear.x};{speed.angular.z}\n".encode())
+    print(f"V{speed.linear.x};{speed.angular.z}\n")
 
 def corect_odom(odom: Pose2D):
     global ser
-    ser.write(f"O{odom.x},{odom.y},{odom.theta}\n".encode())
-    print(f"O{odom.x},{odom.y},{odom.theta}\n")
+    ser.write(f"O{odom.x};{odom.y};{odom.theta}\n".encode())
+    print(f"O{odom.x};{odom.y};{odom.theta}\n")
 
 
 if __name__ == '__main__':
